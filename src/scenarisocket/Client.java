5bca2d4d86f35e5d2e326bb075bd2a7864a1fefd
EnricoLGM <enricolegumi44@gmail.com>
2022-04-26 17:02:45 +0200

    Aggiunta delle classi Server e Client

---
+++ src/scenarisocket/Client.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scenarisocket;
+
+import java.net.*;
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author enric
+ */
+public class Client {
+
+    Socket client;
+    BufferedReader reader;
+    BufferedWriter writer;
+    
+    public Client(InetAddress ip, int porta) {
+        try {
+            client=new Socket(ip, porta);
+            reader=new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer=new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void lettura() {
+        try {
+            System.out.println(reader.readLine());
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Connessione Avvenuta\n");
+        System.out.println("Socket" + client);
+    }
+    
+    public void scrittura(String testo) {
+        try {
+            //scrivo al server un testo dato come parametro alla funzione
+            writer.write(testo+"\n");
+            writer.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void data() {
+        try {
+            //scrivo la richiesta al server
+            writer.write("date\n");
+            writer.flush();
+            
+            //leggo la richiesta e la mostro in output
+            Timestamp time=new Timestamp(Long.parseLong(reader.readLine()));
+            System.out.println(time);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void chiusura() {
+        try {
+            client.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+}
---
+++ src/scenarisocket/Server.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scenarisocket;
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author enric
+ */
+public class Server {
+    
+    ServerSocket serverSock;
+    Socket server;
+    BufferedReader reader;
+    BufferedWriter writer;
+    
+    public Server(int porta) {
+        try {
+            serverSock=new ServerSocket(porta);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void attendi() {
+        try {
+            server=serverSock.accept();
+            reader=new BufferedReader(new InputStreamReader(server.getInputStream()));
+            writer=new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void chiusuraServer() {
+        try {
+            serverSock.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void chiusuraConnessione() {
+        try {
+            server.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void scrittura() {
+        try {
+            writer.write("Benvenuto!\n");
+            writer.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void lettura() {
+        try {
+            System.out.println(reader.readLine());
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void data() {
+        try {
+            if("date".equals(reader.readLine())) {
+                Long tmStmp = System.currentTimeMillis();
+                writer.write(tmStmp+"\n");
+                writer.flush();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
